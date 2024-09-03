fa231ee9629daa849ce48fe82fd740392a25cedb
Dave Arnold <dave@roknsound.com>
2024-09-03 11:10:55 -0700

    chore: Refactor GitHub Actions configuration files

--- github_actions.tf
+++ github_actions.tf
@@ -13,12 +13,6 @@ resource "github_repository_file" "github_actions" {
   }
 }
 
-locals {
-    mod_source = slice(split("-", var.name), 1, length(split("-", var.name)) - 1)
-    provider = mod_source[0]
-    module = join("-", [ for mod_part in range(0, ): mod_source[1]
-}
-
 resource "github_repository_file" "modtest_target_workspaces" {
   for_each   = toset(var.target_workspaces)
   repository = local.github_repo.name
@@ -27,8 +21,8 @@ resource "github_repository_file" "modtest_target_workspaces" {
   content = templatefile(
     "${path.module}/templates/target_workspace.yaml.tpl",
     {
-         workspace = each.value,
-         mod_source = "${tfe_registry_module.registry-module.name}/${tfe_registry_module.registry-module.module_provider}"
+      workspace  = each.value,
+      mod_source = "${tfe_registry_module.registry-module.name}/${tfe_registry_module.registry-module.module_provider}"
     }
   )
   overwrite_on_create = true
