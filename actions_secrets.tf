aa58bbafe7df795bbd77ed096151f3facdf5877b
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-09-04 20:47:21 -0700

    Update actions_secrets.tf

--- actions_secrets.tf
+++ actions_secrets.tf
@@ -57,7 +57,7 @@ locals {
   )
 }
 resource "github_actions_variable" "variable" {
-  for_each      = var.modtest ? tomap({ for _var in local.vars : _var.name => _var.value }) : tomap({})
+  for_each      = tomap({ for _var in local.vars : _var.name => _var.value }}
   repository    = local.github_repo.name
   variable_name = each.key
   value         = each.value
