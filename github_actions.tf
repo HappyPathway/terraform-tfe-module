881ef0d576b04e14fdd1a743fea02b4485ffe86b
Dave Arnold <dave@roknsound.com>
2024-09-12 11:05:34 -0700

    Refactor GitHub Actions configuration and add action badges

--- github_actions.tf
+++ github_actions.tf
@@ -12,8 +12,47 @@ resource "github_repository_file" "github_actions" {
   }
 }
 
+
 locals {
   mod_source = var.create_registry_module ? "${one(tfe_registry_module.registry-module).name}/${one(tfe_registry_module.registry-module).module_provider}" : var.mod_source
+  _action_badges = concat([
+    {
+      name  = "terraform.yaml",
+      title = "Terraform Validation"
+    }
+    ],
+    [
+      for workspace in var.target_workspaces :
+      {
+        name  = "modtest-${workspace.workspace}.yaml",
+        title = "Modtest ${workspace.workspace}"
+      }
+    ]
+  )
+  action_badges = var.github_actions == null ? [
+    for badge in local._action_badges :
+    templatefile("${path.module}/templates/badge.tpl",
+      {
+        title       = title(badge.title),
+        repo_name   = local.github_repo.name,
+        action_name = badge.name
+        server      = var.github_actions.server
+    })
+  ] : []
+}
+
+resource "github_repository_file" "action_badges" {
+  repository          = local.github_repo.name
+  branch              = var.github_default_branch
+  file                = "README.md"
+  content             = join("\n", local.action_badges)
+  overwrite_on_create = true
+  lifecycle {
+    ignore_changes = [
+      branch,
+      content
+    ]
+  }
 }
 
 resource "github_repository_file" "modtest_target_workspaces" {
---
+++ templates/badge.tpl
@@ -0,0 +1,2 @@
+
+[![${title}](https://${server}/${org}/${repo_name}/actions/workflows/${action_name}/badge.svg)](${title}](https://${server}/${org}/${repo_name}/actions/workflows/${action_name})
